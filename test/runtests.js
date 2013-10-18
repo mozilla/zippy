840994fafae67ee82b94752e3679620e4eac707a
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-10-18 12:32:33 -0500

    Gruntified new test runner

--- test/runtests.js
+++ test/runtests.js
@@ -1,9 +1,21 @@
-var reporter = require('nodeunit').reporters.default;
+var nodeunit = require('nodeunit');
 var test = require('./');
 process.chdir(__dirname);
 
-test.start(function() {
-  reporter.run(['suite'], undefined, function() {
-    test.stop();
+module.exports = function(opt) {
+  opt = opt || {onStop: null, reporter: 'default'};
+  var reporter = nodeunit.reporters[opt.reporter];
+  test.start(function() {
+    reporter.run(['suite'], undefined, function() {
+      test.stop();
+      if (opt.onStop) {
+        opt.onStop();
+      }
+    });
   });
-});
+}
+
+if (!module.parent) {
+  // Running as the main script.
+  module.exports();
+}
