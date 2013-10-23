b1e9b0ebb5706eaaad662fb65c191eaab541dccb
Stuart Colville <scolville@mozilla.com>
2013-10-23 22:20:03 +0000

    Add initial style-guide (bug 930252)

--- test/suite/styleguide.test.js
+++ test/suite/styleguide.test.js
@@ -13,12 +13,10 @@ function get(url) {
 
 module.exports = {
 
-  testAutoEscape: function(t) {
-    get('/sellers/%3Cscript%3Ealert%28%27hai%27%29%3C%2Fscript%3E/')
-      .expect(404)
+  testRedirectFromIndex: function(t) {
+    get('/styleguide/index')
+      .expect(301)
       .end(function(err, res) {
-        t.ifError(err);
-        assert.contains('&lt;script&gt;alert(&#39;hai&#39;)&lt;/script&gt;', res.text);
         t.done();
       });
   }
