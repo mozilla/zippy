88791a0be53a77dbe470c05ecda18d2e70be3d0f
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-10-24 17:01:26 -0500

    Add form validation (bug 929072)

--- test/suite/notices.test.js
+++ test/suite/notices.test.js
@@ -36,7 +36,7 @@ exports.testMissingQs = function(t) {
     .expect(409)
     .end(function(err, res) {
       t.ifError(err);
-      t.equal(res.body.code, 'MissingParameter');
+      t.equal(res.body.code, 'InvalidArgument');
       t.done();
     });
 };
