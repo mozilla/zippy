8977ac115d38726c473e8d35c50212ef816977b3
Stuart Colville <scolville@mozilla.com>
2014-02-12 18:31:03 +0000

    Add waitForSelector to text-wrap test

--- uitest/suite/test.product-text-wrap.js
+++ uitest/suite/test.product-text-wrap.js
@@ -9,24 +9,25 @@ startCasper('/payment/confirm');
 casper.test.begin('Product Text Wrapping', {
 
   test: function(test) {
+    casper.waitForSelector('.product-image', function() {
+      // Run the tests.
+      var dataObj = casper.evaluate(function(){
+        // Tell jshint we know $ is a global.
+        /* globals $ */
+        var LONG_PROD_STRING = 'Test AP (fkjsdhkfjhsdkjfhsdkjhfksdjhkjfdsh)';
+        var LONG_AUTHOR_STRING = 'Some-really-long-string-that-shouldn\'t break this';
+        // Inject some long strings.
+        $('.product .title').text(LONG_PROD_STRING);
+        $('.product .author').text(LONG_AUTHOR_STRING);
 
-    // Run the tests.
-    var dataObj = casper.evaluate(function(){
-      // Tell jshint we know $ is a global.
-      /* globals $ */
-      var LONG_PROD_STRING = 'Test AP (fkjsdhkfjhsdkjfhsdkjhfksdjhkjfdsh)';
-      var LONG_AUTHOR_STRING = 'Some-really-long-string-that-shouldn\'t break this';
-      // Inject some long strings.
-      $('.product .title').text(LONG_PROD_STRING);
-      $('.product .author').text(LONG_AUTHOR_STRING);
+        var data = {};
+        data.image_offset =  $('.product-image').offset().top;
+        data.product_offset = $('.product').offset().top;
+        return data;
+      });
 
-      var data = {};
-      data.image_offset =  $('.product-image').offset().top;
-      data.product_offset = $('.product').offset().top;
-      return data;
+      test.assertEqual(dataObj.product_offset, dataObj.image_offset, 'Check image and product tops are aligned (e.g: not wrapped)');
     });
-
-    test.assertEqual(dataObj.product_offset, dataObj.image_offset, 'Check image and product tops are aligned (e.g: not wrapped)');
 
     casper.run(function() {
       test.done();
